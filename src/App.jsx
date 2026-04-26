b631fb8f84eb1887c30bd5be13e541696b831b7f
elviravdiji0 <elviravdiji0@gmail.com>
2026-04-26 20:59:19 +0200

    Listed courses and student info

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+const COURSES = [
+  {
+    id: 1,
+    name: "Client Side Programming",
+    credits: 6,
+    grade: 100,
+    attending: true,
+    difficulty: "Easy"
+  },
+  {
+    id: 2,
+    name: "Databases",
+    credits: 6,
+    grade: 85,
+    attending: true,
+    difficulty: "Moderate"
+  },
+  {
+    id: 3,
+    name: "Operating Systems",
+    credits: 6,
+    grade: 90,
+    attending: true,
+    difficulty: "Moderate"
+  },
+  {
+    id: 4,
+    name: "Applied Probability and Statistics",
+    credits: 6,
+    grade: 75,
+    attending: true,
+    difficulty: "Hard"
+  },
+  {
+    id: 5,
+    name: "Written Communication",
+    credits: 3,
+    grade: 80,
+    attending: true,
+    difficulty: "Easy"
+  },
+  {
+    id: 6,
+    name: "English for Information Technology 2",
+    credits: 3,
+    grade: 85,
+    attending: true,
+    difficulty: "Easy"
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [courses, setCourses] = useState(COURSES);
   return (
-    <>
+    <div>
+      <h2>Elvir Avdiji - #132524</h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Courses</h1>
+        <ul>
+          {courses.map(course => (
+            <div key={course.id} style={{ marginBlock: "3rem", border: "solid 1px black", width: "400px", padding: "1rem", borderRadius: ".5rem" }}>
+              <p>Name: {course.name}</p>
+              <p>Credits: {course.credits}</p>
+              <p>Grade: {course.grade}</p>
+              <p>Attending: {course.attending ? "Yes" : "No"}</p>
+              <p>Difficulty: {course.difficulty}</p>
+            </div>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
